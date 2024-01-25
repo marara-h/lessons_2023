3abbb84ce42a04a5213e9c6285254372834c2dcf
Your Name <you@example.com>
2024-01-25 21:20:04 +0400

    first attempt

--- Bash/script.sh
+++ Bash/script.sh
@@ -1,1 +1,13 @@
- # this is the first file
+#!/bin/bash
+
+echo "Enter username"
+read username
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "secret" ) ]]; then
+echo "valid user"
+else
+echo "invalid user"
+fi
+ 
--- Docker/script.sh
+++ Docker/script.sh
@@ -1,1 +1,12 @@
- # this is the first file
+#!/bin/bash
+
+echo "Enter username"
+read username
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "secret" ) ]]; then
+echo "valid user"
+else
+echo "invalid user"
+fi
